b4e2840cd1a3ff1fd18b1f6a48c6dad135903520
Timothy Nordloh <tnordloh@gmail.com>
2018-06-26 19:58:36 -0600

    I'm committed!

--- lib/markdown_parser.ex
+++ lib/markdown_parser.ex
@@ -13,11 +13,7 @@ defmodule MarkdownParser do
 
   """
   def parse(string) do
-    string
-    |> split_by_block
-    |> Enum.map(fn(x) -> identify_block(x); end)
-    |> Enum.map(fn(x) -> parse_block(x); end)
-    |> Enum.join("\n")
+    find_token(string)
   end
 
   @doc """
@@ -25,12 +21,58 @@ defmodule MarkdownParser do
 
   ## Examples
 
-      iex> MarkdownParser.split_by_block("markdown" <> << 10, 10 >> <> "test" )
-      ["markdown", "test"]
+      iex> MarkdownParser.find_token("markdown test" )
+      "<p>markdown test</p>"
 
   """
-  def split_by_block(string) do
-    String.split(string, "\n\n")
+  def find_token(:complete_token, token, []) do
+    IO.puts("CT1")
+    block_identity = identify_block(token)
+    parse_block(block_identity)
+  end
+
+  def find_token(:complete_token, token, remainder) do
+    IO.puts("CT2")
+    block_identity = identify_block(token)
+    parse_block(block_identity) <> find_token(remainder)
+  end
+
+  def find_token(:partial_token, [next_token | remainder]) do
+    IO.puts("PT1")
+    IO.inspect(next_token)
+
+    if next_token === "" do
+      find_token(:complete_token, next_token, remainder)
+    else
+      find_token(:partial_token, next_token <> hd(remainder), tl(remainder))
+    end
+  end
+
+  def find_token([next_token | remainder]) do
+    IO.puts("PT2")
+
+    if String.match?(next_token, ~r/^[a-zA-Z]/) do
+      IO.puts("s")
+      IO.inspect(next_token)
+      IO.inspect(remainder)
+      IO.puts("e")
+
+      if remainder == [] do
+        find_token(:complete_token, next_token, remainder)
+      else
+        if next_token === "" do
+          find_token(:complete_token, next_token, remainder)
+        else
+          appended_token = next_token <> hd(remainder)
+          find_token([appended_token | tl(remainder)])
+        end
+      end
+    end
+  end
+
+  def find_token(string) do
+    IO.puts("SPLITTER")
+    String.split(string, "\n") |> find_token
   end
 
   def identify_block(string) do
@@ -42,7 +84,6 @@ defmodule MarkdownParser do
   end
 
   def parse_block({:paragraph, string}), do: Parser.to_paragraph(string)
-  def parse_block({:bullets,   string}), do: Parser.to_bullets(string)
-  def parse_block({:header,    string}), do: Parser.to_header(string)
-
+  def parse_block({:bullets, string}), do: Parser.to_bullets(string)
+  def parse_block({:header, string}), do: Parser.to_header(string)
 end
